ba1f39eabf68089e04a7927f9773493828f5e260
aweinr4 <78507140+aweinr4@users.noreply.github.com>
2021-02-15 14:54:48 -0500

    added fourth and seventh mixed invariants

--- src/einsteinpy/symbolic/scalars/mixedinvariants.py
+++ src/einsteinpy/symbolic/scalars/mixedinvariants.py
@@ -299,7 +299,7 @@ class FourthMixedInvariant(MixedInvariant):
             for n in range(np.product(shape[0:2])):
                 o,p = np.unravel_index(n,shape = shape[0:2])
 
-                fourth_mixed += (ricci[i,j]*ricci[k,l]*(weyl_dual[o,i,j,p]*weyl_mix[p,k,l,o] + weyl_mix[o,i,j,p]*weyl_dual[p,k,l,o]))
+                fourth_mixed += (-1*ricci[i,j]*ricci[k,l]*(weyl_dual[o,i,j,p]*weyl_mix[p,k,l,o] + weyl_mix[o,i,j,p]*weyl_dual[p,k,l,o]))
 
 
 
@@ -309,3 +309,71 @@ class FourthMixedInvariant(MixedInvariant):
             parent_metric=parent_metric,
         )
 
+
+
+class SeventhMixedInvariant(MixedInvariant):
+
+    """
+    Class for defining Seventh Mixed Invariant
+    """
+
+    _default = {
+        'name':'SeventhMixedInvariant'
+    }
+
+    @classmethod
+    def from_riemann(cls, riemann, parent_metric=None):
+        """
+        Get Seventh Mixed Invariant calculated from riemann Tensor equation given by:
+
+        ..math:: 
+                \\frac{1}{16}R^{ij}{}R^{kl}{}\\left(C_{oijp}{}C^{o}{}_{kl}{}^{p}{}+\\star{C}_{oijp}{}\\star{C}^{o}{}_{kl}{}^{p}{}\\right)
+
+            where \\star{C} is the dual of the weyl tensor
+
+        Parameters
+        ----------
+        riemann: ~einsteinpy.symbolic.tensors.riemann.RiemannTensor
+            Riemann Tensor
+        parent_metric : ~einsteinpy.symbolic.tensors.metric.MetricTensor or None
+            Corresponding Metric for the SeventhMixedInvariant.
+            Defaults to None.
+
+        """
+        #weyl tensor
+        weyl = WeylTensor.from_riemann(riemann)
+
+        #need dual of weyl
+        weyl_dual = DualWeylTensor.from_weyltensor(weyl)
+
+        #need these forms of dual mixed weyl
+        weyl_dual2 = weyl_dual.change_config('ullu').tensor()
+        weyl_dual1 =weyl_dual.change_config('llll').tensor()
+
+        #need this form of mixed weyl
+        weyl_mix1 = weyl.change_config('llll').tensor()
+        weyl_mix2 = weyl.change_config('ullu').tensor()
+
+
+        #need contravariant form for ricci
+        ricci = RicciTensor.from_riemann(riemann).change_config('uu').tensor()
+
+        if parent_metric is None:
+            parent_metric = riemann.parent_metric
+        
+        shape = weyl_mix1.shape
+        seventh_mixed = 0
+        for t in range(np.product(shape)):
+            i,j,k,l = np.unravel_index(t,shape = shape)
+            for n in range(np.product(shape[0:2])):
+                o,p = np.unravel_index(n,shape = shape[0:2])
+
+                seventh_mixed += ((1/16)*ricci[i,j]*ricci[k,l]*(weyl_mix1[o,i,j,p]*weyl_mix2[o,k,l,p] + weyl_dual1[o,i,j,p]*weyl_dual2[o,k,l,p]))
+
+
+
+        return cls(
+            simplify(seventh_mixed),
+            riemann.syms,
+            parent_metric=parent_metric,
+        )
